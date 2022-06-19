72e19b42a75e5639df453473935bcd5a8a98ac00
Yinon Ozery <yinonozery@gmail.com>
2022-06-19 14:15:32 +0300

    Add edit roles in profile

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -21,29 +21,30 @@ const Popup = (props) => {
 
     return (
         <div>
-            {
-                props.showPopup && (
-                    <div className="outside-background spaced"
-                        ref={popUpRef}
-                        onClick={closePopup}>
-                        {transition((style, item) => item && <animated.div className="popUp box container container--mini" style={style}>
-                            <div className="popUp-content container">
-                                <h1 className='popUp-content__title'>{props.title}</h1>
-                                <p className='popUp-content__description'>{props.description}</p>
-                                <button className="btn--accent">
-                                    <div className="spaced">
-                                        <p>הרשמה</p>
-                                        <div className="btn__icon btn__icon--edit"><MdCheck /></div>
-                                    </div>
-                                </button>
-                            </div>
-                            <MdClose className="popUp__close-btn"
-                                aria-label="Close popup"
-                                onClick={() => props.handlePopup()}
-                            />
-                        </animated.div>)}
-                    </div>
-                )
+            {props.showPopup && (
+                <div className="outside-background spaced"
+                    ref={popUpRef}
+                    onClick={closePopup}>
+                    {transition((style, item) => item && <animated.div className="popUp box container container--mini" style={style}>
+                        <div className="popUp-content container">
+                            <h1 className='popUp-content__title'>{props.title}</h1>
+                            <p className='popUp-content__description'>{props.description}</p>
+                            {props.html}
+
+                            {!props.noButton ? <button className="btn--accent">
+                                <div className="spaced">
+                                    <p>{props.buttonText}</p>
+                                    <div className="btn__icon btn__icon--edit"><MdCheck /></div>
+                                </div>
+                            </button> : null}
+                        </div>
+                        <MdClose className="popUp__close-btn"
+                            aria-label="Close popup"
+                            onClick={() => props.handlePopup()}
+                        />
+                    </animated.div>)}
+                </div>
+            )
             }
         </div >
     )
