294e22972db473d6c2912dbfd390d447fa056818
Amit Hemo <itsamit442@gmail.com>
2022-08-18 16:35:42 +0300

    add delete for updates

--- src/components/UserUpdatesList.js
+++ src/components/UserUpdatesList.js
@@ -1,19 +1,34 @@
 import { useEffect, useState } from 'react';
 import UserUpdate from './UserUpdate';
 import { db } from "./Firebase";
-import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { collection, query, getDocs, orderBy, doc, getDoc } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 function UserUpdatesList() {
 
     const [updates, setUpdates] = useState([]);
-
+    const [isUserAdmin, setIsUserAdmin] = useState(false);
+    const user = getAuth();
+    
     useEffect(() => {
         const getUpdates = async () => {
             const data = await getDocs(query(collection(db, "updates"), orderBy('created')));
-            setUpdates(data.docs.map((doc) => ({ ...doc.data(), uid: doc.ref })));
+            setUpdates(data.docs.map((doc) => ({ ...doc.data(), uid: doc.id })));
         }
         getUpdates();
     }, []);
+
+    useEffect(() => {
+        onAuthStateChanged(user, async () => {
+            const refUser = doc(db, "users", user.currentUser.uid);
+            const doc2 = await getDoc(refUser);
+            if (doc2.data().isAdmin)
+                setIsUserAdmin(true);
+            else
+                setIsUserAdmin(false);
+        })
+    }, [user]);
 
     return (
         <div>
@@ -25,6 +40,8 @@ function UserUpdatesList() {
                         title={updates.title}
                         msg={updates.msg}
                         date={updates.created.toDate().toLocaleDateString('he-IL')}
+                        uid={updates.uid}
+                        isUserAdmin={isUserAdmin}
                     />
                 )
             })}
